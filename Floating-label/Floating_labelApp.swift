c4591be7990c6189f425d07fe0c710a42165041d
mdsrmed <mdsrmed@gmail.com>
2023-09-22 17:47:04 -0400

    Initial Commit

---
+++ Floating-label/Floating_labelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Floating_labelApp.swift
+//  Floating-label
+//
+//  Created by Md Shohidur Rahman on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Floating_labelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
